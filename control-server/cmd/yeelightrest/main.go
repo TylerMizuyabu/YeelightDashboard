20f84f8e7ecffcaebfa0c64d74c3c0491b44c17d
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2020-07-25 15:14:06 -0400

    removed 3rd party lib

--- control-server/cmd/yeelightrest/main.go
+++ control-server/cmd/yeelightrest/main.go
@@ -1,68 +1,37 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
-
-	"github.com/avarabyeu/yeelight"
+	"net"
 )
 
-var discoverCommand string = "\r\nM-SEARCH * HTTP/1.1\r\nMAN: \"ssdp:discover\"\r\nST: wifi_bulb\r\n"
+var discoverCommand string = "M-SEARCH * HTTP/1.1\r\n HOST:239.255.255.250:1982\r\n MAN:\"ssdp:discover\"\r\n ST:wifi_bulb\r\n"
 var address string = "239.255.255.250:1982"
 
 func main() {
-	// udpAddr, err := net.ResolveUDPAddr("udp4", address)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// packetConn, err := net.ListenPacket("udp4", ":0")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer packetConn.Close()
-	// socket := packetConn.(*net.UDPConn)
+	udpAddr, err := net.ResolveUDPAddr("udp4", address)
+	if err != nil {
+		panic(err)
+	}
+	packetConn, err := net.ListenPacket("udp4", ":0")
+	if err != nil {
+		panic(err)
+	}
+	defer packetConn.Close()
+	socket := packetConn.(*net.UDPConn)
 
-	// _, err = socket.WriteToUDP([]byte(discoverCommand), udpAddr)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// var b []byte
-	// for {
-	// 	n, addr, err := socket.ReadFromUDP(b)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// 	fmt.Println("got here")
-	// 	if n > 0 {
-	// 		fmt.Println("n:", n)
-	// 		fmt.Println("addr:", addr)
-	// 		fmt.Println("b:", b)
-	// 	}
-	// }
-	y, err := yeelight.Discover()
+	_, err = socket.WriteToUDP([]byte(discoverCommand), udpAddr)
 	if err != nil {
 		panic(err)
 	}
 
-	on, err := y.GetProp("power")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Power is %s", on[0].(string))
+	buf := make([]byte, 1024)
+	for {
+		n, _, err := socket.ReadFromUDP(buf)
+		if err != nil {
+			panic(err)
+		}
 
-	notifications, done, err := y.Listen()
-	if err != nil {
-		panic(err)
+		fmt.Println("b:", buf[0:n])
 	}
-	go func() {
-		<-time.After(time.Second)
-		done <- struct{}{}
-	}()
-	for n := range notifications {
-		fmt.Println(n)
-	}
-
-	context.Background().Done()
 }
