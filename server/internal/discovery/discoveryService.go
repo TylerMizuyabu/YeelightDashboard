77e7526ae01220300611030e2db624ada617f332
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2020-08-01 21:32:16 -0400

    broke the discover function into two separate functions. One for reading advertisement request over udp and another for writing a discover request over udp

--- server/internal/discovery/discoveryService.go
+++ server/internal/discovery/discoveryService.go
@@ -10,8 +10,10 @@ import (
 var discoverCommand = "M-SEARCH * HTTP/1.1\r\n HOST:239.255.255.250:1982\r\n MAN:\"ssdp:discover\"\r\n ST:wifi_bulb\r\n"
 var address = "239.255.255.250:1982"
 var timeout = time.Second * 3
-var pollingInterval = time.Second * 1
+var pollingInterval = time.Second
 var maxPollingInterval = time.Minute
+var discoverRequestInterval = time.Minute
+var maxDiscoveryRequestInterval = time.Hour
 
 type DiscoveryService struct {
 	failures int
@@ -25,11 +27,6 @@ func NewDiscoveryService() *DiscoveryService {
 
 func (ds *DiscoveryService) Start() chan *types.Yeelight {
 	c := make(chan *types.Yeelight)
-	go ds.discover(c)
-	return c
-}
-
-func (ds *DiscoveryService) discover(c chan *types.Yeelight) {
 	udpAddr, err := net.ResolveUDPAddr("udp4", address)
 	if err != nil {
 		// Think of a better way to handle these errors
@@ -40,27 +37,39 @@ func (ds *DiscoveryService) discover(c chan *types.Yeelight) {
 		panic(err)
 	}
 	socket := packetConn.(*net.UDPConn)
+	go ds.sendDiscoverCommand(socket, udpAddr)
+	go ds.readDiscoveryAdvertisements(socket)
+	return c
+}
+
+func (ds *DiscoveryService) sendDiscoverCommand(socket *net.UDPConn, udpAddr *net.UDPAddr) {
 	socket.SetReadDeadline(time.Now().Add(timeout))
-	// According to the yeelight spec there should be an advertisement request sent out
-	// every hour, or when a new light joins the network. I don't know how much I believe it
-	// though
-	_, err = socket.WriteToUDP([]byte(discoverCommand), udpAddr)
 	for {
-		if err != nil {
+		// According to the yeelight spec there should be an advertisement request sent out
+		// every hour, or when a new light joins the network. I don't know how much I believe it
+		// though
+
+		if _, err := socket.WriteToUDP([]byte(discoverCommand), udpAddr); err != nil {
 			fmt.Println("Error attempting to send discovery request")
-			ds.handleFailure()
+			ds.handleFailure(discoverRequestInterval, maxDiscoveryRequestInterval)
 			continue
 		}
+		ds.failures = 0
+	}
+}
+
+func (ds *DiscoveryService) readDiscoveryAdvertisements(socket *net.UDPConn) {
+	for {
 		rsBuf := make([]byte, 1024)
 		size, _, err := socket.ReadFromUDP(rsBuf)
 		if err != nil {
-			ds.handleFailure()
+			ds.handleFailure(pollingInterval, maxPollingInterval)
 			continue
 		} else if size > 0 {
 			y, err := types.NewYeelightFromDiscoveryResponse(string(rsBuf[0:size]))
 			if err != nil {
 				fmt.Println("Error occurred attempting to decode response")
-				ds.handleFailure()
+				ds.handleFailure(pollingInterval, maxPollingInterval)
 				continue
 			}
 			c <- y
@@ -69,12 +78,12 @@ func (ds *DiscoveryService) discover(c chan *types.Yeelight) {
 	}
 }
 
-func (ds *DiscoveryService) handleFailure() {
+func (ds *DiscoveryService) handleFailure(interval time.Duration, maxDuration time.Duration) {
 	ds.failures++
-	sleepDuration := pollingInterval * time.Duration(ds.failures)
-	if sleepDuration < maxPollingInterval {
+	sleepDuration := interval * time.Duration(ds.failures)
+	if sleepDuration < maxDuration {
 		time.Sleep(sleepDuration)
 	} else {
-		time.Sleep(maxPollingInterval)
+		time.Sleep(maxDuration)
 	}
 }
